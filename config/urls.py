a63d84879bcb9898ec5df24ea9d08a2558f30a9d
«Artyom» <lzspector@mail.ru>
2026-09-05 21:00:05 +0500

    fixed

--- config/urls.py
+++ config/urls.py
@@ -5,3 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
